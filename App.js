f0b98bf8fc256db57e626e44cc717298963fcc6f
Arun Singh <arunsinghsnd@gmail.com>
2021-05-22 00:56:00 +0530

    loaded the custom font oswald and lato

--- App.js
+++ App.js
@@ -3,10 +3,26 @@ import React from "react";
 import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { ThemeProvider } from "styled-components";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 import { theme } from "./src/infrastruture/theme";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
