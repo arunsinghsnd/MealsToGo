08fed4da3c4a64694d89d92427bc7a847e6f181e
Arun Singh <arunsinghsnd@gmail.com>
2021-05-21 22:46:00 +0530

    created the card info component and styled it

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,14 +1,38 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 
-const RestaurantInfo = () => {
+const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    openingNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
   return (
-    <View>
-      <Text>restaurant info</Text>
-    </View>
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Title style={styles.title}>{name}</Title>
+    </Card>
   );
 };
 
 export default RestaurantInfo;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+  },
+  cover: {
+    padding: 20,
+    backgroundColor: "white",
+  },
+  title: {
+    padding: 16,
+  },
+});
