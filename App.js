77c9bd98f79bef6ece28729687d25e99df896ebc
Arun Singh <arunsinghsnd@gmail.com>
2021-05-21 22:09:58 +0530

    serach fields added

--- App.js
+++ App.js
@@ -1,26 +1,16 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View
-          style={{
-            padding: 16,
-            backgroundColor: "green",
-          }}
-        >
-          <Text>Search</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar placeholder="Search" />
         </View>
-        <View
-          style={{
-            padding: 16,
-            backgroundColor: "blue",
-            flex: 1,
-          }}
-        >
+        <View style={styles.list}>
           <Text>List</Text>
         </View>
       </SafeAreaView>
@@ -29,4 +19,18 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "white",
+  },
+  list: {
+    padding: 16,
+    backgroundColor: "blue",
+    flex: 1,
+  },
+});
